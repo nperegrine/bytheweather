edb5fb84a19456fe6f990bdeb4b65353ca5d024f
Peregrine Nkwain <nkwainperegrine@gmail.com>
2023-07-24 14:31:51 +0100

    configure ant-design-vue

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,13 @@ import router from "./router";
 
 import "./assets/main.css";
 
+// Ant design vue
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/reset.css";
+
 const app = createApp(App);
 
+app.use(Antd);
 app.use(createPinia());
 app.use(router);
 
