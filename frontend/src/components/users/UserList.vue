8ea41587e09496fb5e88d9ef604470c4cb601646
Peregrine Nkwain <nkwainperegrine@gmail.com>
2023-07-24 15:55:03 +0100

    fe: added actions column to users list table

--- frontend/src/components/users/UserList.vue
+++ frontend/src/components/users/UserList.vue
@@ -1,44 +1,54 @@
 <template>
-  <a-table :dataSource="dataSource" :columns="columns" />
+  <a-table :dataSource="dataSource" :columns="columns">
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'actions'">
+        <span>
+          <a-button>Edit 一 {{ record.name }}</a-button>
+          <a-divider type="vertical" />
+          <a-button danger>Delete</a-button>
+        </span>
+      </template>
+    </template>
+  </a-table>
 </template>
 
-<script>
-export default {
-  setup() {
-    return {
-      dataSource: [
-        {
-          key: "1",
-          name: "Mike",
-          coordinates: "91,92",
-          weather: "28 degrees",
-        },
-        {
-          key: "2",
-          name: "John",
-          coordinates: "91,97",
-          weather: "26 degrees",
-        },
-      ],
+<script setup>
+import { ref } from "vue";
 
-      columns: [
-        {
-          title: "Name",
-          dataIndex: "name",
-          key: "name",
-        },
-        {
-          title: "Coordinates",
-          dataIndex: "coordinates",
-          key: "coordinates",
-        },
-        {
-          title: "Current Weather",
-          dataIndex: "weather",
-          key: "weather",
-        },
-      ],
-    };
+const dataSource = ref([
+  {
+    key: "1",
+    name: "Mike",
+    coordinates: "91,92",
+    weather: "28 degrees",
   },
-};
+  {
+    key: "2",
+    name: "John",
+    coordinates: "91,97",
+    weather: "26 degrees",
+  },
+]);
+
+const columns = ref([
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Coordinates",
+    dataIndex: "coordinates",
+    key: "coordinates",
+  },
+  {
+    title: "Current Weather",
+    dataIndex: "weather",
+    key: "weather",
+  },
+  {
+    title: "Actions",
+    key: "actions",
+  },
+]);
 </script>
